91dce489ef86e360dbe90a6605fa0a0ac3e2f44a
Unknown <Deadstylebp@gmail.com>
2020-05-20 22:57:48 -0500

    Expand type definitions

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import express from "express";
 import { ApolloServer, gql } from "apollo-server-express";
 // Allow for cross-domain request
-import cors from 'cors';
+import cors from "cors";
 
 // set app variable to express main function
 const app = express();
@@ -13,21 +13,42 @@ app.use(cors());
 // GraphQL schema, set types
 const schema = gql`
   type Query {
+    users: [User!]
+    user(id: ID!): User
     me: User
   }
 
   type User {
+    id: ID!
     username: String!
   }
 `;
 
+// Static data for testing
+let users = {
+  1: {
+    id: "1",
+    username: "Bob Parsons",
+  },
+  2: {
+    id: "2",
+    username: "Dave Davids",
+  },
+};
+
+const me = users[1];
+
 // GraphQL resolvers, set returns
 const resolvers = {
   Query: {
+    users: () => {
+      return Object.values(users);
+    },
+    user: (parent, { id }) => {
+      return users[id];
+    },
     me: () => {
-      return {
-        username: "Bob Parsons",
-      };
+      return me;
     },
   },
 };
